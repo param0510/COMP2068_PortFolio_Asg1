50f60ebab769ef78044397721ac19649d3426cc4
param0510 <paramveer0510@gmail.com>
2023-06-12 18:15:29 -0400

    Comments Added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
+// Index router
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', (req, res, next) => {
+  res.redirect('/home')
+})
+
 router.get('/home', function(req, res, next) {
   res.render('./index/home', { title: 'Home' });
 });
