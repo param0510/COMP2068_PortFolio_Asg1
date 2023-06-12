3d82174ed193a4eb193fb8db19ac03d881889e8a
param0510 <paramveer0510@gmail.com>
2023-06-11 23:40:32 -0400

    Project Phase 1 Complete - Routing and Basic Templates

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('./index/home', { title: 'Home' });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('./index/about', {title: 'About'})
+})
+
+router.get('/projects', (req, res, next) => {
+  res.render('./index/projects', {title: 'My Projects'})
+})
+
+router.get('/contact', (req, res, next) => {
+  res.render('./index/contact', {title: 'Contact Me'})
+})
+module.exports = router;
